6bc5cb63ec66b6c46dd931b94cd91371eaae708f
JessMotta <jessicalimamotta@gmail.com>
2022-04-14 17:32:58 -0300

    create confirmation page to register and linked to register button

--- scripts/register.js
+++ scripts/register.js
@@ -40,5 +40,6 @@ register.addEventListener("click", async function () {
 });
 // função que associa o id do usuário ao id da categoria
 async function linkUserToCategory(userID, categoryValue) {
-  console.log(await associateUserToCategory(userID, categoryValue));
+  await associateUserToCategory(userID, categoryValue);
+  window.location.href = "./confirmation_register.html"
 }

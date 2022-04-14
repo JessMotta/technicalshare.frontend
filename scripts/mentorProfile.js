8c3691adc92bbfec71891968e409adc3583a4e07
JessMotta <jessicalimamotta@gmail.com>
2022-04-14 12:52:50 -0300

    if mentor has a second category will be displayed

--- scripts/mentorProfile.js
+++ scripts/mentorProfile.js
@@ -4,18 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // ler o id do usuário escolhido na url da página
-const urlParams = new URLSearchParams(window.location.search)
-const getId = urlParams.get("id")
+const urlParams = new URLSearchParams(window.location.search);
+const getId = urlParams.get("id");
 
 // id do mentor
-const idMentor = parseInt(getId)
-
+const idMentor = parseInt(getId);
 
 // inicializar as informações do mentor de acordo com o id
 async function initializeInfo() {
   return await findUserById(idMentor)
     .then((dataMentors) => {
       showInfo(dataMentors);
+      console.log(dataMentors);
     })
     .catch((error) => {
       console.log(`Error: ${error}`);
@@ -25,30 +25,40 @@ async function initializeInfo() {
 function showInfo(dataMentors) {
   // armazena as áreas de nome e categoria da tela
   let mentorName = document.getElementById("mentor-name-profile");
-  let category = document.getElementById("category-profile");
-  let skill = document.getElementById("skill-profile")
+  let category_1 = document.getElementById("category-1");
+  let category_2 = document.getElementById("category-2");
 
   // vai receber o id do mentor
   mentorName.textContent = dataMentors.name;
-  category.textContent = dataMentors.email;
-  skill.textContent = dataMentors.skills.skill
-  
-  // mostrar a avaliação da habilidade
-  let rating = parseFloat(dataMentors.skills.rating);
+  category_1.textContent = dataMentors.Categories[0].name;
 
-  // todas as estrelas do HTML
-  const stars = document.querySelectorAll(".star-icon");
-  console.log(stars);
-
-  // para receber a posição correta no array stars
-  let i = rating - 1;
-  
-  // pega o valor do data-avaliacao 
-  let av = stars[i].attributes[1].value;
-  // console.log(av);
-
-  // adiciona a classe ativo se o rating(avaliação) for igual a posição do data-avaliacao
-  if (rating == av) {
-    stars[i].classList.add("ativo");
+  // caso o mentor tenha uma segunda categoria ela será exibida
+  if (dataMentors.Categories.length > 1) {
+    category_2.style.display = "block";
+    category_2.textContent = dataMentors.Categories[1].name;
+  } else {
+    // caso o mentor não tenha segunda categoria
+    category_2.style.display = "none";
   }
+
+  // skill.textContent = dataMentors.skills.skill
+
+  // // mostrar a avaliação da habilidade
+  // let rating = parseFloat(dataMentors.skills.rating);
+
+  // // todas as estrelas do HTML
+  // const stars = document.querySelectorAll(".star-icon");
+  // console.log(stars);
+
+  // // para receber a posição correta no array stars
+  // let i = rating - 1;
+
+  // // pega o valor do data-avaliacao
+  // let av = stars[i].attributes[1].value;
+  // // console.log(av);
+
+  // // adiciona a classe ativo se o rating(avaliação) for igual a posição do data-avaliacao
+  // if (rating == av) {
+  //   stars[i].classList.add("ativo");
+  // }
 }

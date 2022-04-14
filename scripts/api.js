46c1a651597d314080ef6836d2f6acabab1433a2
JessMotta <jessicalimamotta@gmail.com>
2022-04-14 16:46:09 -0300

    add function in api to associate user id to category id

--- scripts/api.js
+++ scripts/api.js
@@ -1,49 +1,63 @@
-const API_URL = 'https://technical-share-20.herokuapp.com/v1';
-const HTTP_METHODS = { GET: 'GET', POST: 'POST', PATCH: 'PATCH', PUT: 'PUT', DELETE: 'DELETE' };
+const API_URL = "https://technical-share-20.herokuapp.com/v1";
+const HTTP_METHODS = {
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
 
-document.addEventListener("DOMContentLoaded", async () => {
-  // Lista todas as categorias
-  console.log('Lista Categorias>>>', await getCategories());
+// document.addEventListener("DOMContentLoaded", async () => {
+//   // Lista todas as categorias
+//   console.log('Lista Categorias>>>', await getCategories());
 
-  // Pesquisa dentre todas as categorias
-  console.log('Pesquisa Categoria >>>', await getCategories('ux'));
+//   // Pesquisa dentre todas as categorias
+//   console.log('Pesquisa Categoria >>>', await getCategories('ux'));
 
-  // Procura uma categoria pelo ID
-  console.log('Busca Categoria pelo ID >>>', await findCategory(2));
+//   // Procura uma categoria pelo ID
+//   console.log('Busca Categoria pelo ID >>>', await findCategory(2));
 
-  // Busca todos os usuários que tem uma categoria, pelo ID
-  console.log('Retorna Usuários que detém uma categoria, através do ID da Categoria >>>', await findUsersByCategory(2));
+//   // Busca todos os usuários que tem uma categoria, pelo ID
+//   console.log('Retorna Usuários que detém uma categoria, através do ID da Categoria >>>', await findUsersByCategory(2));
 
-  // Lista todos os usuários
-  console.log('Lista Usuários >>>', await getUsers());
+//   // Lista todos os usuários
+//   console.log('Lista Usuários >>>', await getUsers());
 
-  // Procura um usuário pelo ID 
-  console.log('Busca Usuário pelo ID >>>', await findUserById(1));
+//   // Procura um usuário pelo ID
+//   console.log('Busca Usuário pelo ID >>>', await findUserById(1));
 
-  // Cria um usuário e o retorna 
-  console.log('Cria Usuário, parâmetros: {email: string; name: string; password: string;} >>>', await createUser({
-    name: 'Afonso2',
-    email: 'afons2o@gmail.com',
-    password: '12345678'
-  }));
-});
+//   // Cria um usuário e o retorna
+//   console.log('Cria Usuário, parâmetros: {email: string; name: string; password: string;} >>>', await createUser({
+//     name: 'Afonso2',
+//     email: 'afons2o@gmail.com',
+//     password: '12345678'
+//   }));
+
+//   console.log("Associa usuário a categoria >>>", await associateUserToCategory(10, 4))
+
+//   // Lista todos os usuários
+//   console.log('Lista Usuários >>>', await getUsers());
+// });
 
 function setFetchConfig(method, body) {
   return {
     method,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-    body: body ? body : undefined
-  }
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: body ? body : undefined,
+  };
 }
 
 async function getCategories(nameToQuery) {
   const config = setFetchConfig(HTTP_METHODS.GET);
-  const searchString = nameToQuery ? `?name=${nameToQuery}` : '';
+  const searchString = nameToQuery ? `?name=${nameToQuery}` : "";
 
   return await fetch(`${API_URL}/categories${searchString}`, config)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(err => console.warn('Error on getCategories: ', err));
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.warn("Error on getCategories: ", err));
 }
 
 async function findCategory(categoryId) {
@@ -51,12 +65,12 @@ async function findCategory(categoryId) {
 
   if (categoryId) {
     return await fetch(`${API_URL}/categories/${categoryId}`, config)
-      .then(response => response.json())
-      .then(data => data)
-      .catch(err => console.warn('Error on findCategory: ', err));
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((err) => console.warn("Error on findCategory: ", err));
   }
 
-  return console.warn('Category ID is required');
+  return console.warn("Category ID is required");
 }
 
 async function findUsersByCategory(categoryId) {
@@ -64,21 +78,21 @@ async function findUsersByCategory(categoryId) {
 
   if (categoryId) {
     return await fetch(`${API_URL}/categories/${categoryId}/users`, config)
-      .then(response => response.json())
-      .then(data => data)
-      .catch(err => console.warn('Error on findUsersByCategory: ', err));
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((err) => console.warn("Error on findUsersByCategory: ", err));
   }
 
-  return console.warn('Category ID is required');
+  return console.warn("Category ID is required");
 }
 
 async function getUsers() {
   const config = setFetchConfig(HTTP_METHODS.GET);
 
   return await fetch(`${API_URL}/users`, config)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(err => console.warn('Error on getUsers: ', err));
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.warn("Error on getUsers: ", err));
 }
 
 async function findUserById(userId) {
@@ -86,19 +100,34 @@ async function findUserById(userId) {
 
   if (userId) {
     return await fetch(`${API_URL}/users/${userId}`, config)
-      .then(response => response.json())
-      .then(data => data)
-      .catch(err => console.warn('Error on findUserById: ', err));
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((err) => console.warn("Error on findUserById: ", err));
   }
 
-  return console.warn('User ID is required');
+  return console.warn("User ID is required");
 }
 
 async function createUser({ name, email, password }) {
-  const config = setFetchConfig(HTTP_METHODS.POST, JSON.stringify({ name, email, password }));
+  const config = setFetchConfig(
+    HTTP_METHODS.POST,
+    JSON.stringify({ name, email, password })
+  );
 
   return await fetch(`${API_URL}/users`, config)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(err => console.warn('Error on createUser: ', err));
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.warn("Error on createUser: ", err));
+}
+
+async function associateUserToCategory(userId, categoryId) {
+  const config = setFetchConfig(HTTP_METHODS.PATCH);
+
+  return await fetch(
+    `${API_URL}/users/${userId}/associate/${categoryId}`,
+    config
+  )
+    .then((response) => response.json)
+    .then((data) => data)
+    .catch((err) => console.warn("Error to update user: ", err));
 }
--- scripts/register.js
+++ scripts/register.js
@@ -1,30 +1,44 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   initializeForm();
-// });
-
 let mentorName = document.getElementById("name");
-let email = document.getElementById("email");
-
+let mentorEmail = document.getElementById("email");
 let category = document.getElementById("category");
 
-
+// por enquanto não armazena esses valores no banco de dados
 let mentor = document.getElementById("mentor");
 let mentored = document.getElementById("mentored");
 
+// armazena o id do usuário, e o id da categoria associada
+let userID;
+let categoryValue;
+
+// botão para registrar usuario
 let register = document.getElementById("register");
 
-function send() {
-
-if(mentorName != ""){}
-  console.log(mentorName.value);
-  console.log(email.value);
-  console.log(specialty.value);
-  console.log(skill.value);
-  if (mentor.checked) {
-    console.log(mentor.value);
+register.addEventListener("click", async function () {
+  // checa se todos os campos estão preenchidos
+  if (
+    mentorName.value != "" &&
+    mentorEmail.value != "" &&
+    category.value != 0
+  ) {
+    //   cria o usuário
+    await createUser({
+      name: mentorName.value,
+      email: mentorEmail.value,
+      password: "12345678",
+    });
+    // procura o usuario pelo nome criado para poder identificar seu id gerado no sistema
+    await getUsers().then((data) => {
+      data.forEach((user) => {
+        if (user.name === mentorName.value) {
+          userID = user.id;
+          categoryValue = parseInt(category.value);
+          linkUserToCategory(userID, categoryValue);
+        }
+      });
+    });
   }
-
-  if (mentored.checked) {
-    console.log(mentored.value);
-  }
+});
+// função que associa o id do usuário ao id da categoria
+async function linkUserToCategory(userID, categoryValue) {
+  console.log(await associateUserToCategory(userID, categoryValue));
 }
